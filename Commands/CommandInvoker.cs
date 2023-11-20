a97075b0a49db3621e97d551715f9beb7f097058
Student <irozina2006@gmail.com>
2023-11-20 13:51:24 +1000

    Добавьте файлы проекта.

---
+++ Commands/CommandInvoker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help_me.Commands
+{
+    internal class CommandInvoker
+    {
+        public CommandManager CommandManager { get; set; }
+
+        public void Run()
+        {
+            string command;
+            do
+            {
+                Console.WriteLine("Введите команду");
+                command = Console.ReadLine();
+                CommandManager.Execute(command);
+            }
+            while (command != "exit");
+            if (command == "help"):
+                Console.WriteLine(List<comm>
+
+        }
+    }
+}
+
---
+++ Commands/CommandManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class CommandManager
+{
+    Dictionary<string,(string descr, CommandUser)> commands = new();
+    public void Execute(string? command)
+    {
+        if (commands.ContainsKey(command))
+        {
+            Console.WriteLine(commands["test"].descr);
+            commands[command].Item2.Execute();
+        }
+    }
+
+    public void RegisterDescr(string descr, CommandUser commandUser)
+    {
+        commands.Add("test", ("описание команды", commandUser));
+    }
+ 
+    public void RegisterCommand(string command, CommandUser commandUser)
+    {
+        commands.Add(command, ("Добавление команды", commandUser));
+    }
+
+    public void ListCommands (string text)
+    {
+        foreach(var command in commands.Keys)
+        {
+            Console.WriteLine($"{command} - {commands["test"].descr}");
+        }
+    }
+}
